e96988b9598c742cfb4aea8144a342cf2626d25b
oviyaan07 <oviyaansingh04@gmail.com>
2024-11-03 13:01:53 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+temp=float(input("Enter temperature: "))
+if temp>25:
+    print(f"{temp}°C is suitable for wearing light clothes")
+elif temp==25:
+    print(f"{temp}°C is suitable for wearing light clothes")
+else:
+    print(f"{temp}°C is not suitable for wearing light clothes")
